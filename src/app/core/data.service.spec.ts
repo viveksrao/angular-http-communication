722484d5589ce102984870bcd6d0d694289b9331
Vivekanand Rao <vivekanand.rao@gmail.com>
2021-02-13 19:01:32 -0800

    Demo: Testing HTTP Requests and Responses.

--- src/app/core/data.service.spec.ts
+++ src/app/core/data.service.spec.ts
@@ -1,15 +1,56 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+  TestRequest,
+} from "@angular/common/http/testing";
 
-import { DataService } from './data.service';
+import { DataService } from "./data.service";
+import { Book } from "app/models/book";
+import { BookTrackerError } from "../models/book-tracker-error";
 
-describe('DataService', () => {
+describe("DataService Tests", () => {
+  let dataService: DataService;
+  let httpTestingController: HttpTestingController;
+  let testBooks: Book[] = [
+    {
+      bookID: 2,
+      title: "Winnie-the-Pooh",
+      author: "A. A. Milne",
+      publicationYear: 1926,
+    },
+    {
+      bookID: 3,
+      title: "Where the Wild Things Are",
+      author: "Maurice Sendak",
+      publicationYear: 1963,
+    },
+    {
+      bookID: 4,
+      title: "The Hobbit",
+      author: "J. R. R. Tolkien",
+      publicationYear: 1937,
+    },
+  ];
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DataService]
+      imports: [HttpClientTestingModule],
+      providers: [DataService],
     });
+    dataService = TestBed.get(DataService);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
-  it('should be created', inject([DataService], (service: DataService) => {
-    expect(service).toBeTruthy();
-  }));
+  it("should GET all books", () => {
+    dataService.getAllBooks().subscribe((data: Book[] | BookTrackerError) => {
+      expect((<Book[]>data).length).toBe(3);
+    });
+    let booksRequest: TestRequest = httpTestingController.expectOne(
+      "/api/books"
+    );
+    expect(booksRequest.request.method).toEqual("GET");
+    booksRequest.flush(testBooks);
+    httpTestingController.verify();
+  });
 });
